fb04e5fcf8f31561ce12dad3acc0105a1d5acd7b
Morten Poul Jensen <mpj@signifly.com>
2018-05-04 13:13:07 +0200

    Add SchedulingTasksTest

---
+++ tests/SchedulingTasksTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Signifly\SchedulingTasks\Test;
+
+use Signifly\SchedulingTasks\TaskLoader;
+use Illuminate\Console\Scheduling\Schedule;
+
+class SchedulingTasksTest extends TestCase
+{
+    /** @test */
+    function it_loads_the_scheduling_tasks()
+    {
+        // TO DO
+    }
+}
